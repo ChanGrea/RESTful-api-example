069f748f6f9d0cb3ef7a982b2740a9e21fb3aa26
ChanGrea <k39335@naver.com>
2020-08-27 23:08:23 +0900

    :sparkles: added: get/update post

--- src/main/java/io/changrea/restapiexample/service/PostsService.java
+++ src/main/java/io/changrea/restapiexample/service/PostsService.java
@@ -1,7 +1,10 @@
 package io.changrea.restapiexample.service;
 
+import io.changrea.restapiexample.domain.posts.Posts;
 import io.changrea.restapiexample.domain.posts.PostsRepository;
+import io.changrea.restapiexample.web.dto.PostsResponseDto;
 import io.changrea.restapiexample.web.dto.PostsSaveRequestDto;
+import io.changrea.restapiexample.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
---
+++ src/main/java/io/changrea/restapiexample/web/dto/PostsResponseDto.java
@@ -0,0 +1,18 @@
+package io.changrea.restapiexample.web.dto;
+
+import io.changrea.restapiexample.domain.posts.Posts;
+
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
