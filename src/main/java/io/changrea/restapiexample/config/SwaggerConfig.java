ba70167d506f59895db8deff9e4eeb579ab36b7d
ChanGrea <k39335@naver.com>
2020-09-06 04:25:29 +0900

    :wrench: added: swagger

---
+++ src/main/java/io/changrea/restapiexample/config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package io.changrea.restapiexample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+    private String version;
+    private String title;
+
+    @Bean
+    public Docket apiV1() {
+        version = "V1";
+        title = "RESTful API Example";
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .groupName(version)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("io.changrea.restapiexample"))
+                .paths(PathSelectors.ant("/v1/api/**"))
+                .build()
+                .apiInfo(apiInfo(title, version));
+    }
+
+    private ApiInfo apiInfo(String title, String version) {
+        return new ApiInfo(
+                title,
+                "ChanGrea's RESTful API Example Docs",
+                version,
+                "none",
+                new Contact("Contact Me", "https://github.com/changrea", "k39335@naver.com"),
+                "Licenses",
+                "https://github.com/changrea",
+                new ArrayList<>());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
--- src/main/java/io/changrea/restapiexample/web/PostsApiController.java
+++ src/main/java/io/changrea/restapiexample/web/PostsApiController.java
@@ -6,6 +6,7 @@ import io.changrea.restapiexample.service.PostsService;
 import io.changrea.restapiexample.web.dto.PostsResponseDto;
 import io.changrea.restapiexample.web.dto.PostsSaveRequestDto;
 import io.changrea.restapiexample.web.dto.PostsUpdateRequestDto;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
@@ -15,13 +16,19 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "PostsApiController V1")
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/v1/api")
 public class PostsApiController {
 
     private final PostsService postsService;
 
-    @GetMapping("/api/v1/posts")
+    @ApiOperation(value = "Get All Posts", notes = "모든 포스트를 가져옵니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공")
+    })
+    @GetMapping("/posts")
     public ResponseEntity<CollectionModel<PostsResponseDto>> getAllPosts() {
         List<PostsResponseDto> posts = postsService.getAllPosts();
         Link link = linkTo(methodOn(PostsApiController.class)
@@ -34,8 +41,12 @@ public class PostsApiController {
         );
     }
 
-    @GetMapping("/api/v1/posts/{id}")
-    public ResponseEntity<EntityModel<PostsResponseDto>> findById(@PathVariable Long id) {
+    @ApiOperation(value = "Get Post for id", notes = "포스트를 가져옵니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공")
+    })
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<EntityModel<PostsResponseDto>> findById(@ApiParam(value = "Post ID", required = true, example = "1") @PathVariable Long id) {
         PostsResponseDto post = postsService.findById(id);
         Link selfLink = linkTo(methodOn(PostsApiController.class).findById(id)).withSelfRel();
         Link updateLink = linkTo(methodOn(PostsApiController.class)).withRel("update");
@@ -49,7 +60,11 @@ public class PostsApiController {
         return ResponseEntity.ok().body(result);
     }
 
-    @PostMapping("/api/v1/posts")
+    @ApiOperation(value = "Save Post", notes = "포스트를 저장합니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공")
+    })
+    @PostMapping("/posts")
     public ResponseEntity<EntityModel<Long>> save(@RequestBody PostsSaveRequestDto requestDto) {
         Long id = postsService.save(requestDto);
         Link selfLink = linkTo(methodOn(PostsApiController.class).save(requestDto)).withSelfRel();
@@ -66,8 +81,12 @@ public class PostsApiController {
         return ResponseEntity.ok().body(result);
     }
 
-    @PutMapping("/api/v1/posts/{id}")
-    public ResponseEntity<EntityModel<Long>> update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+    @ApiOperation(value = "Get Post for ID", notes = "포스트를 수정합니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공")
+    })
+    @PutMapping("/posts/{id}")
+    public ResponseEntity<EntityModel<Long>> update(@ApiParam(value = "Post ID", required = true, example = "1") @PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         Long updatedId = postsService.update(id, requestDto);
         Link selfLink = linkTo(methodOn(PostsApiController.class).update(id, requestDto)).withSelfRel();
         Link getLink = linkTo(methodOn(PostsApiController.class)).withRel("get");
@@ -81,8 +100,12 @@ public class PostsApiController {
         return ResponseEntity.ok().body(result);
     }
 
-    @DeleteMapping("/api/v1/posts/{id}")
-    public ResponseEntity<EntityModel<Boolean>> delete(@PathVariable Long id) {
+    @ApiOperation(value = "Delete Post for ID", notes = "포스트를 삭제합니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공")
+    })
+    @DeleteMapping("/posts/{id}")
+    public ResponseEntity<EntityModel<Boolean>> delete(@ApiParam(value = "Post ID", required = true, example = "1") @PathVariable Long id) {
         boolean isRemoved = postsService.delete(id);
         Link selfLink = linkTo(methodOn(PostsApiController.class).delete(id)).withSelfRel();
 
